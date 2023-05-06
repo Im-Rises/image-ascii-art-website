5b87bb6bfb2b5c8db2d5f22a9cb3fbb8c1b9d3e6
Im-Rises <quentin-morel88@hotmail.com>
2023-05-06 00:14:58 -0400

    New version

--- src/components/ImageAsciiPanel.tsx
+++ src/components/ImageAsciiPanel.tsx
@@ -1,21 +1,20 @@
-import React, {useRef, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {ImageAscii, ArtTypeEnum} from 'image-ascii-art';
 import CopyImage from '../images/copy.svg';
-import demoImage from '../images/demoImage.png';
 import './ImageAsciiPanel.scss';
 
 const ImageAsciiPanel = () => {
 	// Define the ascii art chars per line
-	const charsPerLine = 100;
+	const charsPerLine = 200;
 	const [charsPerColumn, setCharsPerColumn] = useState(0);
+	const [image, setImage] = useState<HTMLImageElement>();
+	const [isImageReady, setIsImageReady] = useState(false);
 	const [useColor, setUseColor] = useState(false);
-	const inputRef = useRef<HTMLInputElement>(null);
-	const preTagRef = useRef<HTMLPreElement>(null);
-	const parentRef = useRef<HTMLDivElement>(null);
-	// const [isImageSelected, setIsImageSelected] = useState(false);
-	const [imageSrc, setImageSrc] = useState<string>('');
 
-	// Calculate the chars per column according to the aspect ratio of the image
+	const preTagRef = useRef<HTMLPreElement>(null);
+	const inputRef = useRef<HTMLInputElement>(null);
+	const parentRef = useRef<HTMLDivElement>(null);
+
 	const calculateCharsPerColumn = (image: HTMLImageElement) => Math.round(charsPerLine * (image.height / image.width));
 
 	// Handle the copy to clipboard button click
@@ -28,48 +27,77 @@ const ImageAsciiPanel = () => {
 		}
 	};
 
+	const handleImageChange = () => {
+		if (inputRef.current?.files?.length) {
+			const file = inputRef.current.files[0];
+			const reader = new FileReader();
+			reader.onload = () => {
+				if (reader.result !== '') {
+					const img = new Image();
+					img.src = reader.result as string;
+					img.onload = () => {
+						setCharsPerColumn(calculateCharsPerColumn(img));
+						setIsImageReady(true);
+						setImage(img);
+					};
+				}
+			};
+
+			reader.readAsDataURL(file);
+		}
+	};
+
 	const toggleColor = () => {
 		setUseColor(!useColor);
 	};
 
-	const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const file = e.target.files![0];
-		const reader = new FileReader();
-		reader.readAsDataURL(file);
-		reader.onloadend = () => {
-			setImageSrc(reader.result as string);
-		};
+	const ejectImage = () => {
+		setIsImageReady(false);
+		setImage(undefined);
+		setUseColor(false);
 	};
+
+	// // Default image (Uncomment it to use it)
+	// useEffect(() => {
+	// 	const img = new Image();
+	// 	img.src = ImageDemo;
+	// 	img.onload = () => {
+	// 		setCharsPerColumn(calculateCharsPerColumn(img));
+	// 		setIsImageReady(true);
+	// 		setImage(img);
+	// 	};
+	// }, []);
 
 	return (
 		<div>
-			{imageSrc
+			{isImageReady
 				? (
 					<>
 						<div className={'image-ascii-panel'}>
 							<div ref={parentRef} className={'image-ascii-holder'}>
 								<ImageAscii
-									// imageSrc={imageSrc}
-									imageSrc={demoImage}
+									image={image!}
 									parentRef={parentRef}
+									artType={useColor ? ArtTypeEnum.ASCII_COLOR_BG_IMAGE : ArtTypeEnum.ASCII}
 									charsPerLine={charsPerLine}
 									charsPerColumn={charsPerColumn}
 									fontColor={'white'}
 									backgroundColor={'black'}
-									artType={useColor ? ArtTypeEnum.ASCII_COLOR_BG_IMAGE : ArtTypeEnum.ASCII}
 									preTagRef={preTagRef}
 								/>
 							</div>
-						</div>
-						<div>
-							<button
-								className={`${'Button-Toggle-Mode'} ${useColor ? 'Button-Toggle-BW' : 'Button-Toggle-Color'}`}
-								onClick={toggleColor}>
-							</button>
-							<button className={'Button-Copy-Clipboard'}
-								onClick={async () => copyToClipboard(preTagRef.current!.innerText)}>
-								<img src={CopyImage} alt={'CopyLogoImage'}/>
-							</button>
+							<div>
+								<button
+									className={`${'Button-Toggle-Mode'} ${useColor ? 'Button-Toggle-BW' : 'Button-Toggle-Color'}`}
+									onClick={toggleColor}>
+								</button>
+								<button className={'Button-Copy-Clipboard'}
+									onClick={async () => copyToClipboard(preTagRef.current!.innerText)}>
+									<img src={CopyImage} alt={'CopyLogoImage'}/>
+								</button>
+								<button className={'Button-Eject-Image'} onClick={ejectImage}>
+								</button>
+							</div>
 						</div>
 					</>
 				)
@@ -78,7 +106,7 @@ const ImageAsciiPanel = () => {
 						<h1 className={'app-title'}>Image ASCII</h1>
 						<div className={'image-input-container'}>
 							<input ref={inputRef} style={{display: 'none'}} type='file' accept='image/*'
-								onChange={handleInputChange}/>
+								onChange={handleImageChange}/>
 							<button className={'image-input-button'} onClick={() => {
 								inputRef.current?.click();
 							}}>Select image
