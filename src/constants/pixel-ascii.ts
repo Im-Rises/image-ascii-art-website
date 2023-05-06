7148fb30f7f979172c28854f09d3726570d72aed
Im-Rises <quentin-morel88@hotmail.com>
2023-05-06 00:25:07 -0400

    Corrected git link

--- src/constants/pixel-ascii.ts
+++ src/constants/pixel-ascii.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/naming-convention */
-const GITHUB_URL = 'https://github.com/Im-Rises/video-stream-ascii-webcam';
-const GITHUB_LINK_TEXT = 'Im-Rises/video-stream-ascii-webcam';
+const GITHUB_URL = 'https://github.com/Im-Rises/image-ascii-art-website';
+const GITHUB_LINK_TEXT = 'Im-Rises/image-ascii-art-website';
 const AUTHOR = 'Im-Rises';
 
 export {GITHUB_URL, GITHUB_LINK_TEXT, AUTHOR};
