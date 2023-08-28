c0152c6f96fcf1d627791b84f4779c914b0c3d7a
Im-Rises <quentin-morel88@hotmail.com>
2023-08-28 19:25:58 -0400

    change github link

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ import {AUTHOR, GITHUB_LINK_TEXT, GITHUB_URL} from './constants/pixel-ascii';
 
 const App = () => (
 	<div className='App'>
-		<GitHubProjectPanel link={GITHUB_URL}
-			author={AUTHOR}/>
 		<ImageAsciiPanel/>
 	</div>
 );
